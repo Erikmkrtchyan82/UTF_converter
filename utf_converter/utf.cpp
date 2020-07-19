2743e5d547a6103dfda698242dbc6b90a99b7c92
Erikmkrtchyan82 <erik.mkrtchyan82@gmail.com>
2020-07-19 20:52:31 +0400

    utf_covert function takes not formatted text

--- utf_converter/utf.cpp
+++ utf_converter/utf.cpp
@@ -7,7 +7,7 @@
 
 UTF find_utf( const string& encoding ) {
 
-	string lowercase=encoding;
+	string lowercase = encoding;
 
 	for_each( lowercase.begin(), lowercase.end(), []( char& c ) {c = tolower( c ); } );
 
@@ -49,147 +49,24 @@ void check_invalid_inputs( char* argv[], ifstream& input_file, ofstream& output_
 		throw std::invalid_argument( error_message );
 }
 
-void decode_utf_8( vector<unsigned int>& text, ifstream& input_file ) {
+void convert( UTF source, UTF target, ifstream& ifs, ofstream& ofs ) {
 
-	UTF_8_type character;
-	auto byte_count = sizeof( character );
-	string error_message = "Input file is not in UTF-8 format\n";
+	if ( source == UTF::UTF_8 ) {
+		auto input = read_from_file<UTF_8_type>( ifs );
 
-	//	Checks, if leading bits, after first byte, is 10
-	auto is_valid_utf_8 = []( const UTF_8_type& c ) {return ( ( c >> 6 ) == 0x2 ); };
-
-	while ( input_file.read( (char*)&character, byte_count ) ) {
-		//	If the number of significant bits is no more than seven, 
-		//	it means that character was encoded in one byte
-		if ( ( character >> 7 ) == 0x00 ) {
-			text.push_back( character );
-		}
-		//	If characer's leading three bits are 110,
-		//	means that character was encoded in two bytes
-		else if ( ( character >> 5 ) == 0x6 ) {
-			auto first = ( character ^ 0xC0 ) << 8;
-
-			input_file.read( (char*)&character, byte_count );
-
-			if ( !is_valid_utf_8( character ) ) {
-				throw std::invalid_argument( error_message );
-			}
-
-			auto second = ( character ^ 0x80 ) << 2;
-			auto all = ( first | second ) >> 2;
-
-			text.push_back( all );
-		}
-		//	If characer's leading four bits are 1110,
-		//	means that character was encoded in three bytes 
-		else if ( ( character >> 4 ) == 0xE ) {
-			UTF_8_type point[ 2 ];
-
-			input_file.read( (char*)point, byte_count );
-			input_file.read( (char*)( point + 1 ), byte_count );
-
-			if ( !is_valid_utf_8( point[ 0 ] ) || !is_valid_utf_8( point[ 1 ] ) ) {
-				throw std::invalid_argument( error_message );
-			}
-
-			auto first = ( character ^ 0xE0 ) << 16;
-			auto second = ( point[ 0 ] ^ 0x80 ) << 2 << 8;
-			auto third = ( point[ 1 ] ^ 0x80 ) << 2;
-			auto all = ( ( ( first | second ) >> 2 ) | third ) >> 2;
-
-			text.push_back( all );
-		}
-		//	If characer's leading five bits are 11110,
-		//	means that character was encoded in four bytes
-		else if ( ( character >> 3 ) == 0x1E ) {
-			UTF_8_type point[ 3 ];
-
-			input_file.read( (char*)point, byte_count );
-			input_file.read( (char*)( point + 1 ), byte_count );
-			input_file.read( (char*)( point + 2 ), byte_count );
-
-			if ( !is_valid_utf_8( point[ 0 ] ) ||
-				!is_valid_utf_8( point[ 1 ] ) ||
-				!is_valid_utf_8( point[ 2 ] ) ) {
-				throw std::invalid_argument( error_message );
-			}
-
-			auto first = ( character ^ 0xF0 ) << 16 << 8;
-			auto second = ( point[ 0 ] ^ 0x80 ) << 2 << 16;
-			auto third = ( point[ 1 ] ^ 0x80 ) << 2 << 8;
-			auto forth = ( point[ 2 ] ^ 0x80 ) << 2;
-			auto all = ( ( ( ( ( first | second ) >> 2 ) | third ) >> 2 ) | forth ) >> 2;
-
-			text.push_back( all );
-		}
-		else {
-			throw std::invalid_argument( error_message );
-		}
+		find_target_and_convert( input, target, ofs );
+		return;
 	}
-}
+	if ( source == UTF::UTF_16 ) {
+		auto input = read_from_file<UTF_16_type>( ifs );
 
-void decode_utf_16( vector<unsigned int>& text, ifstream& input_file ) {
-
-	UTF_16_type character;
-	auto byte_count = sizeof( character );
-	string error_message = "Input file is not in UTF-16 format\n";
-
-	//	Checks, if bytes, after first byte, starts with 110111(b)
-	auto is_valid_utf_16 = []( const UTF_16_type& c ) {return ( ( c >> 10 ) == 0x37 ); };
-
-	while ( input_file.read( (char*)( &character ), byte_count ) ) {
-		//	If first byte is'n start with 1101_10..
-		if ( ( character & 0xD800 ) != 0xD800 ) {
-			//	and if leading bit is 1
-			//	means that this character is not valid UTF-16 format
-			if ( ( character >> 15 ) == 0x1 ) {
-				throw std::invalid_argument( error_message );
-			}
-			else {
-				text.push_back( character );
-			}
-		}
-		else {
-			auto high = character - 0xD800;
-
-			input_file.read( (char*)( &character ), byte_count );
-
-			if ( !is_valid_utf_16( character ) ) {
-				throw std::invalid_argument( error_message );
-			}
-
-			auto low = character - 0xDC00;
-			auto all = ( ( high << 10 ) | low ) + 0x10000;
-
-			text.push_back( all );
-		}
+		find_target_and_convert( input, target, ofs );
+		return;
 	}
-}
+	if ( source == UTF::UTF_32 ) {
+		auto input = read_from_file<UTF_32_type>( ifs );
 
-void decode_utf_32( vector<unsigned int>& text, ifstream& input_file ) {
-
-	UTF_32_type character;
-	auto byte_count = sizeof( character );
-	string error_message = "Input file is not in UTF-32 format\n";
-
-	while ( input_file.read( (char*)&character, byte_count ) ) {
-		if ( ( character >> 16 >> 5 ) != 0 )
-			throw std::invalid_argument( error_message );
-		text.push_back( character );
-	}
-}
-
-void read_from_file( UTF& first_type, vector<unsigned int>& code_point, ifstream& input_file )
-{
-	switch ( first_type ) {
-	case UTF::UTF_8:
-		decode_utf_8( code_point, input_file );
-		break;
-	case UTF::UTF_16:
-		decode_utf_16( code_point, input_file );
-		break;
-	case UTF::UTF_32:
-		decode_utf_32( code_point, input_file );
-		break;
+		find_target_and_convert( input, target, ofs );
+		return;
 	}
 }
--- utf_converter/utf.h
+++ utf_converter/utf.h
@@ -28,19 +28,22 @@ UTF find_utf( const string& );
 //	If everything is okay does nothing
 void check_invalid_inputs( char* [], ifstream&, ofstream& );
 
-//	Throws exception, if format of text in input file is not UTF-8
-void decode_utf_8( vector<unsigned int>&, ifstream& );
+void convert( UTF, UTF, ifstream&, ofstream& );
 
-//	Throws exception, if format of text in input file is not UTF-16
-void decode_utf_16( vector<unsigned int>&, ifstream& );
+template<class T>
+vector<T> read_from_file( ifstream& input_file ) {
 
-//	Throws exception, if format of text in input file is not UTF-32
-void decode_utf_32( vector<unsigned int>&, ifstream& );
+	vector<T> vector;
+	T c;
 
-void read_from_file( UTF&, vector<unsigned int>&, ifstream& );
+	while ( input_file.read( (char*)&c, sizeof( c ) ) ) {
+		vector.emplace_back( c );
+	}
+	return vector;
+}
 
-template<typename InputIterator>
-void encode_utf_8( vector<unsigned int>::iterator& begin, vector<unsigned int>::iterator& end, InputIterator& out ) {
+template<typename OutputIterator>
+void encode_utf_8( vector<unsigned int>::iterator begin, vector<unsigned int>::iterator end, OutputIterator& out ) {
 
 	for ( ; begin != end; ++begin, ++out ) {
 		//	Needs one byte to encode
@@ -53,7 +56,7 @@ void encode_utf_8( vector<unsigned int>::iterator& begin, vector<unsigned int>::
 			auto second = ( *begin & 0x3F ) | 0x80;
 
 			*out = first;
-			*( ++out ) = second;
+			*++out = second;
 		}
 		//	Needs three bytes to encode
 		else if ( *begin <= 0x7FFF ) {
@@ -62,8 +65,8 @@ void encode_utf_8( vector<unsigned int>::iterator& begin, vector<unsigned int>::
 			auto third = ( *begin & 0x3F ) | 0x80;
 
 			*out = first;
-			*( ++out ) = second;
-			*( ++out ) = third;
+			*++out = second;
+			*++out = third;
 		}
 		//	Needs four bytes to encode
 		else if ( *begin <= 0xFFFF ) {
@@ -73,16 +76,16 @@ void encode_utf_8( vector<unsigned int>::iterator& begin, vector<unsigned int>::
 			auto forth = ( *begin & 0x3F ) | 0x80;
 
 			*out = first;
-			*( ++out ) = second;
-			*( ++out ) = third;
-			*( ++out ) = forth;
+			*++out = second;
+			*++out = third;
+			*++out = forth;
 		}
 	}
 }
 
 //	Throws exception
-template<typename InputIterator>
-void encode_utf_16( vector<unsigned int>::iterator& begin, vector<unsigned int>::iterator& end, InputIterator& out ) {
+template<typename OutputIterator>
+void encode_utf_16( vector<unsigned int>::iterator begin, vector<unsigned int>::iterator end, OutputIterator& out ) {
 
 	for ( ; begin != end; ++begin, ++out ) {
 		if ( *begin >= 0xD800 && *begin <= 0xDFFF ) {
@@ -99,7 +102,7 @@ void encode_utf_16( vector<unsigned int>::iterator& begin, vector<unsigned int>:
 			auto low = ( copy & 0x3FF ) + 0xDC00;
 
 			*out = high;
-			*( ++out ) = low;
+			*++out = low;
 		}
 		//	Needs two bytes to encode
 		else {
@@ -108,8 +111,8 @@ void encode_utf_16( vector<unsigned int>::iterator& begin, vector<unsigned int>:
 	}
 }
 
-template<typename InputIterator>
-void encode_utf_32( vector<unsigned int>::iterator& begin, vector<unsigned int>::iterator& end, InputIterator& out ) {
+template<typename OutputIterator>
+void encode_utf_32( vector<unsigned int>::iterator begin, vector<unsigned int>::iterator end, OutputIterator& out ) {
 	for ( ; begin != end; ++begin, ++out ) {
 		*out = *begin;
 	}
@@ -117,8 +120,142 @@ void encode_utf_32( vector<unsigned int>::iterator& begin, vector<unsigned int>:
 
 template<typename InputIterator>
 void write_in_file( InputIterator begin, InputIterator end, ofstream& output_file ) {
-	for ( ; begin != end && *begin != 0; ++begin ) {
-		output_file.write( (char*)( &*begin ), sizeof( typename std::iterator_traits<InputIterator>::value_type ) );
+	for ( ; begin != end; ++begin ) {
+		if ( *begin != 0 )
+			output_file.write( (char*)( &*begin ), sizeof( typename std::iterator_traits<InputIterator>::value_type ) );
+	}
+}
+
+template <typename InputIterator, typename OutputIterator>
+void decode_utf_8( InputIterator begin, InputIterator end, OutputIterator out ) {
+	
+	string error_message = "Input file is not in UTF-8 format\n";
+
+	//	Checks, if leading bits, after first byte, is 10
+	auto is_valid_utf_8 = []( const UTF_8_type& c ) {return ( ( c >> 6 ) == 0x2 ); };
+
+	while ( begin != end ) {
+		//	If the number of significant bits is no more than seven, 
+		//	it means that character was encoded in one byte
+		if ( ( *begin >> 7 ) == 0x00 ) {
+			*out = *begin;
+		}
+		//	If characer's leading three bits are 110,
+		//	means that character was encoded in two bytes
+		else if ( ( *begin >> 5 ) == 0x6 ) {
+			auto first = ( *begin ^ 0xC0 ) << 8;
+
+			++begin;
+
+			if ( !is_valid_utf_8( *begin ) ) {
+				throw std::invalid_argument( error_message );
+			}
+
+			auto second = ( *begin ^ 0x80 ) << 2;
+			auto all = ( first | second ) >> 2;
+
+			*out = all;
+		}
+		//	If characer's leading four bits are 1110,
+		//	means that character was encoded in three bytes 
+		else if ( ( *begin >> 4 ) == 0xE ) {
+			UTF_8_type point[ 2 ];
+
+			point[ 0 ] = *++begin;
+			point[ 1 ] = *++begin;
+
+			if ( !is_valid_utf_8( point[ 0 ] ) || !is_valid_utf_8( point[ 1 ] ) ) {
+				throw std::invalid_argument( error_message );
+			}
+
+			auto first = ( *begin ^ 0xE0 ) << 16;
+			auto second = ( point[ 0 ] ^ 0x80 ) << 2 << 8;
+			auto third = ( point[ 1 ] ^ 0x80 ) << 2;
+			auto all = ( ( ( first | second ) >> 2 ) | third ) >> 2;
+
+			*out = all;
+		}
+		//	If characer's leading five bits are 11110,
+		//	means that character was encoded in four bytes
+		else if ( ( *begin >> 3 ) == 0x1E ) {
+			UTF_8_type point[ 3 ];
+
+			point[ 0 ] = *++begin;
+			point[ 1 ] = *++begin;
+			point[ 2 ] = *++begin;
+
+			if ( !is_valid_utf_8( point[ 0 ] ) ||
+				!is_valid_utf_8( point[ 1 ] ) ||
+				!is_valid_utf_8( point[ 2 ] ) ) {
+				throw std::invalid_argument( error_message );
+			}
+
+			auto first = ( *begin ^ 0xF0 ) << 16 << 8;
+			auto second = ( point[ 0 ] ^ 0x80 ) << 2 << 16;
+			auto third = ( point[ 1 ] ^ 0x80 ) << 2 << 8;
+			auto forth = ( point[ 2 ] ^ 0x80 ) << 2;
+			auto all = ( ( ( ( ( first | second ) >> 2 ) | third ) >> 2 ) | forth ) >> 2;
+
+			*out = all;
+		}
+		else {
+			throw std::invalid_argument( error_message );
+		}
+		++begin;
+		out++;
+	}
+}
+
+template <typename InputIterator, typename OutputIterator>
+void decode_utf_16( InputIterator begin, InputIterator end, OutputIterator out ) {
+
+	string error_message = "Input file is not in UTF-16 format\n";
+
+	//	Checks, if bytes, after first byte, starts with 110111(b)
+	auto is_valid_utf_16 = []( const UTF_16_type& c ) {return ( ( c >> 10 ) == 0x37 ); };
+
+	while ( begin != end ) {
+		//	If first byte is'n start with 1101_10..
+		if ( ( *begin & 0xD800 ) != 0xD800 ) {
+			//	and if leading bit is 1
+			//	means that this character is not valid UTF-16 format
+			if ( ( *begin >> 15 ) == 0x1 ) {
+				throw std::invalid_argument( error_message );
+			}
+			else {
+				*out = *begin;
+			}
+		}
+		else {
+			auto high = *begin - 0xD800;
+
+			++begin;
+
+			if ( !is_valid_utf_16( *begin ) ) {
+				throw std::invalid_argument( error_message );
+			}
+
+			auto low = *begin - 0xDC00;
+			auto all = ( ( high << 10 ) | low ) + 0x10000;
+
+			*out = all;
+		}
+		++begin;
+		out++;
+	}
+
+}
+
+template <typename InputIterator, typename OutputIterator>
+void decode_utf_32( InputIterator begin, InputIterator end, OutputIterator out ) {
+	string error_message = "Input file is not in UTF-32 format\n";
+
+	while ( begin != end ) {
+		if ( ( *begin >> 16 >> 5 ) != 0 )
+			throw std::invalid_argument( error_message );
+		*out = *begin;
+		++begin;
+		out++;
 	}
 }
 
@@ -126,19 +263,55 @@ void write_in_file( InputIterator begin, InputIterator end, ofstream& output_fil
 template <typename InputIterator, typename OutputIterator>
 void utf_convert( InputIterator begin, InputIterator end, OutputIterator out ) {
 
-	string error_message = "Something went wrong in function \"utf_convert\"\n";
+	vector<unsigned int> code_points;
+	std::back_insert_iterator <vector<unsigned int>> it( code_points );
 
+	// Decoding
+	if ( typeid( typename std::iterator_traits<InputIterator>::value_type ) == typeid( UTF_8_type ) ) {
+		decode_utf_8( begin, end, it );
+	}
+	else if ( typeid( typename std::iterator_traits<InputIterator>::value_type ) == typeid( UTF_16_type ) ) {
+		decode_utf_16( begin, end, it );
+	}
+	else if ( typeid( typename std::iterator_traits<InputIterator>::value_type ) == typeid( UTF_32_type ) ) {
+		decode_utf_32( begin, end, it );
+	}
+
+	// Encoding
 	if ( typeid( typename std::iterator_traits<OutputIterator>::value_type ) == typeid( UTF_8_type ) ) {
-		encode_utf_8( begin, end, out );
+		encode_utf_8( code_points.begin(), code_points.end(), out );
 		return;
 	}
 	if ( typeid( typename std::iterator_traits<OutputIterator>::value_type ) == typeid( UTF_16_type ) ) {
-		encode_utf_16( begin, end, out );
+		encode_utf_16( code_points.begin(), code_points.end(), out );
 		return;
 	}
 	if ( typeid( typename std::iterator_traits<OutputIterator>::value_type ) == typeid( UTF_32_type ) ) {
-		encode_utf_32( begin, end, out );
+		encode_utf_32( code_points.begin(), code_points.end(), out );
 		return;
 	}
-	throw std::invalid_argument( error_message );
 }
+
+template<class T>
+void find_target_and_convert( vector<T>& input, UTF target, ofstream& ofs ) {
+
+	if ( target == UTF::UTF_8 ) {
+		vector<UTF_8_type> target( 4 * input.size() );
+		utf_convert( input.begin(), input.end(), target.begin() );
+		write_in_file( target.begin(), target.end(), ofs );
+		return;
+	}
+	if ( target == UTF::UTF_16 ) {
+		vector<UTF_16_type> target( 4 * input.size() );
+		utf_convert( input.begin(), input.end(), target.begin() );
+		write_in_file( target.begin(), target.end(), ofs );
+		return;
+	}
+	if ( target == UTF::UTF_32 ) {
+		vector<UTF_32_type> target( 4 * input.size() );
+		utf_convert( input.begin(), input.end(), target.begin() );
+		write_in_file( target.begin(), target.end(), ofs );
+		return;
+	}
+}
+
